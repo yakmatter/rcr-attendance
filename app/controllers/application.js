7cc6964f83ec5a676da0b417856fed528f17483b
yakmatter <yakspam@mac.com>
2020-02-15 09:19:35 -0800

    add nav for admin user

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,17 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 
 export default Controller.extend({
   header: service(),
   session: service(),
   router: service(),
+  activeRoute: reads('router.currentRouteName'),
+  eventsRouteIsActive: computed('activeRoute', function() {
+    return this.get('activeRoute').startsWith('home.events');
+  }),
+
   shouldShowHomeButton: computed('router.currentRouteName', function() {
     return this.get('router.currentRouteName') !== 'launcher';
   }),
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -41,8 +41,6 @@ export default Controller.extend({
     hiddenElement.target = '_blank';
     hiddenElement.download = `${filename}.csv`;
     hiddenElement.click();
-    // link.click();
-    // document.removeChild(hiddenElement);
   },
 
   actions: {
