c5ca6ca120e41d0ae6f22d2163db8ffc708b0b49
yakmatter <yakspam@mac.com>
2020-02-15 09:39:17 -0800

    only allow "create dummy events" on localhost

--- app/controllers/home/programs/program/teams/team/events/index.js
+++ app/controllers/home/programs/program/teams/team/events/index.js
@@ -3,11 +3,16 @@ import { inject as service } from '@ember/service';
 import { computed }  from '@ember/object';
 import { reads } from '@ember/object/computed';
 import moment from 'moment';
+import config from 'rcr-attendance/config/environment';
 
 export default Controller.extend({
   store: service(),
   session: service(),
   isAdmin: reads('session.user.isAdmin'),
+  init() {
+    this._super(...arguments);
+    this.set('canCreateDummyEvents', config.apiHost.includes('localhost'));
+  },
   events: computed('model.[]', function() {
     return this.get('model').sortBy('startTime').reverse();
   }),
@@ -18,6 +23,9 @@ export default Controller.extend({
       });
     },
     createDummyEvents() {
+      if (!this.get('canCreateDummyEvents')) {
+        return;
+      }
       let date = moment().startOf('day').add(7, 'hours');
       while (moment().endOf('day').subtract(5, 'hours').isAfter(date)) {
         const event = {
