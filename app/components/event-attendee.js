dc202ba7f1103351a5726c01fe22933dea1d2794
yakmatter <yakspam@mac.com>
2020-02-17 10:24:58 -0800

    fix tests for event-attendee

--- app/components/event-attendee.js
+++ app/components/event-attendee.js
@@ -1,21 +1,33 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { computed }  from '@ember/object';
+import { A } from '@ember/array';
 
 export default Component.extend({
   tagName: '',
   session: service(),
+  init() {
+    this._super(...arguments);
+    this.set('relatedCheckedIn', A(this.get('attendance.relatedAttendances')).mapBy('attended'));
+
+  },
   isVisitingSkater: computed('event.teams.[]', 'attendance.skater.teams.[]', function() {
     const eventTeamIds = this.get('event').hasMany('teams').ids();
     const skaterTeamIds = this.get('attendance.skater').hasMany('teams').ids();
 
     return skaterTeamIds.every(skaterTeamId => eventTeamIds.includes(skaterTeamId) === false);
   }),
-  // attendanceCount: computed('event')
+  markOffSkates: () => {},
+  markAttendance: () => {},
+  showRelatedEvents: () => {},
+  isCheckedIn: computed('relatedCheckedIn', function() {
+    return this.get('relatedCheckedIn').any(checkedIn => checkedIn === true);
+  }),
   actions: {
     markAttendance(attendance, checked) {
       attendance.set('attended', checked);
       this.markAttendance(attendance);
+      this.set('relatedCheckedIn', A(this.get('attendance.relatedAttendances')).mapBy('attended'));
     },
     markOffSkates(attendance, checked) {
       attendance.set('offSkates', checked);
--- tests/integration/components/event-attendee-test.js
+++ tests/integration/components/event-attendee-test.js
@@ -1,63 +1,124 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
+
+const SKATER = () => {
+  return EmberObject.create({
+    displayName: 'roland fallwell',
+    team: EmberObject.create({
+      logo: './assets/images/voodoo-dolls.png',
+      className: 'voodoo-dolls'
+    })
+  });
+};
+
+const ATTENDANCE = (addSkater = false, attended = false, isToday = true, name = 'event 1') => {
+  return EmberObject.create({
+    attended,
+    name,
+    event: EmberObject.create({
+      isToday,
+      name
+    }),
+    shouldShowRelatedEvents: false,
+    skater: addSkater ? SKATER() : null
+  });
+};
 
 module('Integration | Component | event-attendee', function(hooks) {
   setupRenderingTest(hooks);
   hooks.beforeEach(function() {
-    this.set('attendance', EmberObject.create({
-      skater: EmberObject.create({
-        displayName: 'roland fallwell',
-        team: EmberObject.create({
-          logo: './assets/images/voodoo-dolls.png',
-          className: 'voodoo-dolls'
-        })
-      }),
-      attended: true,
-      event: EmberObject.create({
-        isToday: true
-      })
-    }));
-
-  });
-
-  test('it renders attended current event', async function(assert) {
-    await render(hbs`{{event-attendee attendance=attendance}}`);
-
-    assert.ok(this.element.querySelector('.event-attendee-team').className.includes(this.get('attendance.skater.team.className')));
-    assert.equal(this.element.querySelector('.event-attendee-team img').getAttribute('src'), this.get('attendance.skater.team.logo'));
-    assert.equal(this.element.querySelector('.event-attendee-skater-name').textContent.trim(), this.get('attendance.skater.displayName'));
-    assert.equal(this.element.querySelector('.event-attendee-attended').textContent.trim(), 'Checked In');
-    assert.ok(this.element.querySelector('.event-attendee-attended .checkbox.checked'));
-    assert.ok(this.element.querySelector('.event-attendee'));
+    let attendance = ATTENDANCE(true);
+    attendance.set('relatedAttendances', [ attendance ]);
+    this.set('attendance', attendance);
+    this.set('showRelatedEvents', function() {
+      this.get('attendance').toggleProperty('shouldShowRelatedEvents');
+    });
   });
 
   test('it renders not attended current event', async function(assert) {
-    this.set('attendance.attended', false);
-    await render(hbs`{{event-attendee attendance=attendance}}`);
+    await render(hbs`
+      {{event-attendee attendance=attendance showRelatedEvents=showRelatedEvents}}
+    `);
 
-    assert.ok(this.element.querySelector('.event-attendee-team').className.includes(this.get('attendance.skater.team.className')));
-    assert.equal(this.element.querySelector('.event-attendee-team img').getAttribute('src'), this.get('attendance.skater.team.logo'));
-    assert.equal(this.element.querySelector('.event-attendee-skater-name').textContent.trim(), this.get('attendance.skater.displayName'));
-    assert.equal(this.element.querySelector('.event-attendee-attended').textContent.trim(), 'Checked In');
-    assert.notOk(this.element.querySelector('.event-attendee-attended .checkbox.checked'));
-    assert.ok(this.element.querySelector('.event-attendee'));
+    assert.dom('.event-attendee').exists();
+    assert.dom('.event-attendee-team').hasClass(this.get('attendance.skater.team.className'));
+    assert.dom('.event-attendee-team img').hasAttribute('src', this.get('attendance.skater.team.logo'));
+    assert.dom('.event-attendee-skater-name').hasText(this.get('attendance.skater.displayName'));
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Check In');
+
+    await click('.checkin-button');
+    assert.dom('.event-attendee-attendances-row-0 .event-attendee-event-name').hasText('event 1');
+    assert.dom('.event-attendee-attendances-row-0 .is-present-checkbox input').isNotChecked();
+
+    assert.dom('.event-attendee-attendances-row-0 .off-skates-checkbox input').isDisabled();
+
+    await click('.event-attendee-attendances-row-0 .is-present-checkbox');
+    assert.dom('.event-attendee-attendances-row-0 .is-present-checkbox input').isChecked();
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Checked In');
+
+    assert.dom('.event-attendee-attendances-row-0 .off-skates-checkbox input').isNotChecked();
+  });
+
+  test('it renders attended current event', async function(assert) {
+    this.set('attendance.attended', true);
+    // this.set('attendance.relatedAttendances.firstObject.attended', true);
+    await render(hbs`{{event-attendee attendance=attendance showRelatedEvents=showRelatedEvents}}`);
+
+    assert.dom('.event-attendee').exists();
+    assert.dom('.event-attendee-team').hasClass(this.get('attendance.skater.team.className'));
+    assert.dom('.event-attendee-team img').hasAttribute('src', this.get('attendance.skater.team.logo'));
+    assert.dom('.event-attendee-skater-name').hasText(this.get('attendance.skater.displayName'));
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Checked In');
+
+    await click('.checkin-button');
+    assert.dom('.event-attendee-attendances-row-0 .event-attendee-event-name').hasText('event 1');
+    assert.dom('.event-attendee-attendances-row-0 .is-present-checkbox input').isChecked();
+
+    assert.dom('.event-attendee-attendances-row-0 .off-skates-checkbox input').isNotChecked();
+    await click('.event-attendee-attendances-row-0 .off-skates-checkbox');
+    assert.dom('.event-attendee-attendances-row-0 .off-skates-checkbox input').isChecked();
+
+    await click('.event-attendee-attendances-row-0 .is-present-checkbox');
+    assert.dom('.event-attendee-attendances-row-0 .is-present-checkbox input').isNotChecked();
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Check In');
+
+    assert.dom('.event-attendee-attendances-row-0 .off-skates-checkbox input').isDisabled();
+  });
+
+
+
+  test('it renders "checked in" if any related event is attended', async function(assert) {
+    this.get('attendance.relatedAttendances').addObject(ATTENDANCE(false, true, true, 'event 2'));
+
+    await render(hbs`{{event-attendee attendance=attendance showRelatedEvents=showRelatedEvents}}`);
+
+    assert.dom('.event-attendee').exists();
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Checked In');
+
+    await click('.checkin-button');
+    assert.dom('.event-attendee-attendances-row-0 .event-attendee-event-name').hasText('event 1');
+    assert.dom('.event-attendee-attendances-row-0 .is-present-checkbox input').isNotChecked();
+
+    assert.dom('.event-attendee-attendances-row-1 .event-attendee-event-name').hasText('event 2');
+    assert.dom('.event-attendee-attendances-row-1 .is-present-checkbox input').isChecked();
+
+    await click('.event-attendee-attendances-row-1 .is-present-checkbox');
+    assert.dom('.event-attendee-attendances-row-1 .is-present-checkbox input').isNotChecked();
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Check In');
   });
 
   test('it renders attended past event', async function(assert) {
+    this.set('attendance.attended', true);
     this.get('attendance.event').setProperties({
       isToday: false,
       hasEnded: true
     });
     await render(hbs`{{event-attendee attendance=attendance}}`);
-
-    assert.ok(this.element.querySelector('.event-attendee-team').className.includes(this.get('attendance.skater.team.className')));
-    assert.equal(this.element.querySelector('.event-attendee-team img').getAttribute('src'), this.get('attendance.skater.team.logo'));
-    assert.equal(this.element.querySelector('.event-attendee-skater-name').textContent.trim(), this.get('attendance.skater.displayName'));
-    assert.equal(this.element.querySelector('.event-attendee-attended').textContent.trim(), 'Attended');
-    assert.ok(this.element.querySelector('.event-attendee'));
+    assert.dom('.event-attendee').exists();
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Attended');
   });
 
   test('it renders not attended past event', async function(assert) {
@@ -65,28 +126,20 @@ module('Integration | Component | event-attendee', function(hooks) {
       isToday: false,
       hasEnded: true
     });
-    this.set('attendance.attended', false);
     await render(hbs`{{event-attendee attendance=attendance}}`);
-
-    assert.ok(this.element.querySelector('.event-attendee-team').className.includes(this.get('attendance.skater.team.className')));
-    assert.equal(this.element.querySelector('.event-attendee-team img').getAttribute('src'), this.get('attendance.skater.team.logo'));
-    assert.equal(this.element.querySelector('.event-attendee-skater-name').textContent.trim(), this.get('attendance.skater.displayName'));
-    assert.equal(this.element.querySelector('.event-attendee-attended').textContent.trim(), 'Did not attend');
-    assert.ok(this.element.querySelector('.event-attendee'));
+    assert.dom('.event-attendee').exists();
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Did Not Attend');
   });
 
   test('it renders attended future event', async function(assert) {
+    this.set('attendance.attended', true);
     this.get('attendance.event').setProperties({
       isToday: false,
       hasEnded: false
     });
     await render(hbs`{{event-attendee attendance=attendance}}`);
-
-    assert.ok(this.element.querySelector('.event-attendee-team').className.includes(this.get('attendance.skater.team.className')));
-    assert.equal(this.element.querySelector('.event-attendee-team img').getAttribute('src'), this.get('attendance.skater.team.logo'));
-    assert.equal(this.element.querySelector('.event-attendee-skater-name').textContent.trim(), this.get('attendance.skater.displayName'));
-    assert.equal(this.element.querySelector('.event-attendee-attended').textContent.trim(), '...');
-    assert.ok(this.element.querySelector('.event-attendee'));
+    assert.dom('.event-attendee').exists();
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Checked In');
   });
 
   test('it renders not attended future event', async function(assert) {
@@ -94,13 +147,8 @@ module('Integration | Component | event-attendee', function(hooks) {
       isToday: false,
       hasEnded: false
     });
-    this.set('attendance.attended', false);
     await render(hbs`{{event-attendee attendance=attendance}}`);
-
-    assert.ok(this.element.querySelector('.event-attendee-team').className.includes(this.get('attendance.skater.team.className')));
-    assert.equal(this.element.querySelector('.event-attendee-team img').getAttribute('src'), this.get('attendance.skater.team.logo'));
-    assert.equal(this.element.querySelector('.event-attendee-skater-name').textContent.trim(), this.get('attendance.skater.displayName'));
-    assert.equal(this.element.querySelector('.event-attendee-attended').textContent.trim(), '...');
-    assert.ok(this.element.querySelector('.event-attendee'));
+    assert.dom('.event-attendee').exists();
+    assert.dom('.event-attendee-attended .checkin-button').hasText('Check In');
   });
 });
